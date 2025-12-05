5d0546bcd89a9d1b3ae7c1e98e98fb14d3b88c98
Theo Lee <theolee72@naver.com>
2025-12-05 16:16:48 +0900

    feat:mainpage force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@/utils/supabase/server";
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const supabase = await createClient();
   const {
